00ccd6778945c88f2800a901be22e00395183dc4
Victor Winberg <dat14vwi@student.lu.se>
2017-07-09 23:59:59 +0200

    json integration

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var app = angular.module('app', []);
+const app = angular.module('app', [])
+
 app.controller('apartmentsCtrl', function($scope, $http) {
   $http.get("magasinet.json").then(function (response) {
-    $scope.data = response.data;
-  });
+
+    const data = response.data
+    data.sort((a, b) => parseInt(b.rent) - parseInt(a.rent))
+    $scope.data = data
+  })
 })
