a5605a1ba0fd48674c019be68b0a8354f39b72c4
VictorWinberg <dat14vwi@student.lu.se>
2017-09-20 11:13:22 +0200

    magasinet.json sorted

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const app = angular.module('app', [])
 
 app.controller('apartmentsCtrl', function($scope, $http) {
   $http.get("magasinet.json").then(function (response) {
-    const data = response.data
-    data.sort((a, b) => parseInt(a.lghnummer) - parseInt(b.lghnummer))
-    $scope.data = data
+    $scope.data = response.data
   })
 })
 
